b4b1fbfd5c822563e6360f1953eb697b53971253
Fraser <Fraser@192.168.2.8>
2013-05-01 11:33:12 +0800

    Main Code

---
+++ src/me/mrfrase3/chgp/CHGP.java
@@ -0,0 +1,70 @@
+import com.laytonsmith.abstraction.MCLocation;
+import com.laytonsmith.abstraction.MCPlayer;
+import com.laytonsmith.annotations.api;
+import com.laytonsmith.core.CHVersion;
+import com.laytonsmith.core.ObjectGenerator;
+import com.laytonsmith.core.Static;
+import com.laytonsmith.core.constructs.CArray;
+import com.laytonsmith.core.constructs.CBoolean;
+import com.laytonsmith.core.constructs.Construct;
+import com.laytonsmith.core.constructs.Target;
+import com.laytonsmith.core.environments.CommandHelperEnvironment;
+import com.laytonsmith.core.environments.Environment;
+import com.laytonsmith.core.exceptions.ConfigRuntimeException;
+import com.laytonsmith.core.functions.AbstractFunction;
+import com.laytonsmith.core.functions.Exceptions;
+import me.ryanhamshire.GriefPrevention.Claim;
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+
+public class CHGP {
+    @api(environments = {CommandHelperEnvironment.class})
+    public static class has_gp_buildperm extends AbstractFunction {
+        
+        public Exceptions.ExceptionType[] thrown() {
+            return null;
+        }
+        
+        public boolean isRestricted() {
+            return true;
+        }
+        
+        public Boolean runAsync() {
+            return false;
+        }
+        
+        public Construct exec(Target t, Environment environment, Construct... args) throws ConfigRuntimeException {
+			Player player = (Player)args[0];
+            //Player player = Bukkit.getPlayer(args[0].val());
+			CArray array = (CArray)args[1];
+            MCLocation loc = ObjectGenerator.GetGenerator().location(array, null, t);
+            
+            Claim claim = GriefPrevention.instance.dataStore.getClaimAt((Location)loc.getHandle(), false, null);
+			String errorMessage = claim.allowBuild(player);
+            
+            if (errorMessage == null){
+                return new CBoolean(true, t);
+			} else {
+                return new CBoolean(false, t);
+			}
+        }
+        
+        public String getName() {
+            return getClass().getSimpleName();
+        }
+        
+        public Integer[] numArgs() {
+            return new Integer[]{2};
+        }
+        
+        public String docs() {
+            return "boolean {player, location} See if a player can build at a given location.";
+        }
+        
+        public CHVersion since() {
+            return CHVersion.V3_3_1;
+        }
+    }
+}
